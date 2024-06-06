d133e1944b3b37a0e439ed9c86cb69e1cdff68ae
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2024-06-06 11:28:07 +0530

    updated by anujesh

--- routes/purchaseOrder.route.js
+++ routes/purchaseOrder.route.js
@@ -5,7 +5,6 @@ import { ProductWisePurchaseReport, deletePurchaseOrder, purchaseOrder, purchase
 const router = express.Router();
 const upload = multer({ dest: "public/Images/" })
 
-
 router.post("/save-purchase-order", purchaseOrder)
 router.get("/view-purchase-order-history/:id/:database", purchaseOrderHistory)
 router.get("/view-purchase-order-history-by-id/:id", purchaseOrderHistoryByOrderId);
@@ -13,6 +12,5 @@ router.put("/update-purchase-order/:id", updatePurchaseOrder);
 router.put("/update-purchase-order-status/:id", updatePurchaseOrderStatus);
 router.post("/product-purchase-report/:database", ProductWisePurchaseReport)
 router.delete("/delete-purchase-order/:id", deletePurchaseOrder);
-
 
 export default router;
